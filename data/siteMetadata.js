c4994ededb6f3145d8e703c3368dd4f87da70247
trongtomo <binguyenquoctrong@gmail.com>
2023-07-27 14:42:29 +0700

    change data in author

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@ const siteMetadata = {
   title: 'trongtomo',
   author: 'trongtomo',
   headerTitle: 'trongtomo',
-  description: 'Software Engineer at Phu My Phuong',
+  description: 'Software Engineer',
   language: 'en-us',
   theme: 'dark', // system, dark or light
   siteUrl: 'https://trongtomo.com',
